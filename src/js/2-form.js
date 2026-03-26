8fff6ac8fff603e15ff36c2a4d1606e4a7725bbd
djdanilko <djdanilko02@gmail.com>
2026-03-26 19:19:21 +0100

    first

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,39 @@
-console.log('Form');
+const formData = { email: '', message: '' };
+
+const inputForm = document.querySelector('.feedback-form');
+
+inputForm.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+const savedData = localStorage.getItem('feedback-form-state');
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  inputForm.elements.email.value = formData.email;
+  inputForm.elements.message.value = formData.message;
+}
+
+inputForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+
+  formData.email = '';
+  formData.message = '';
+
+  inputForm.reset();
+});
